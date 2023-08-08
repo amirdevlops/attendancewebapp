ddd223b525f2242472156784346a5eed8e2cb11c
amirdevlops <137190254+amirdevlops@users.noreply.github.com>
2023-08-08 20:35:19 +0530

    Add files via upload

---
+++ javascripts/home.js
@@ -0,0 +1,6 @@
+let creatsheetrefer = document.getElementById("creatsheetrefer");
+
+creatsheetrefer.addEventListener("click" , ()=>{
+    creatsheetrefer.textContent = "Good Choice"
+    window.location.href = "../htmls/create.html"
+})
